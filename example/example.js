ce2f3d12c29a50363e25b8e93240639962dd98c9
Anton <anton@adc.sh>
2018-09-08 03:03:12 +0300

    write full svg

--- example/example.js
+++ example/example.js
@@ -1,12 +1,19 @@
+import { svg } from '@svag/lib'
 import { writeFileSync } from 'fs'
 /* start example */
 import toolbar from '../src'
 
 const res = toolbar({
   title: '👾Terminal',
-  width: 800,
+  width: 400,
 })
 
 console.log(res)
 /* end example */
-writeFileSync('images/toolbar.svg', res)
+const image = svg({
+  content: res,
+  height: 50,
+  width: 400,
+  stretch: false,
+})
+writeFileSync('images/toolbar.svg', image)
